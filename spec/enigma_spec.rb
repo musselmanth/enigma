147f3d5d04a86b9181599e5394d1faca6d648892
Thomas Musselman <tmusselman@gmail.com>
2022-08-04 16:00:08 -0600

    test: enigma.encrypt with all arguments provided and no edge cases

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Enigma do
     expect(enigma).to be_an Enigma
   end
 
+  it 'encripts a message when the key and date are provided' do
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
 end
