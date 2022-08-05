9be403ba40e9ff4374327d5ce16e52ce9e9e513e
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 15:40:40 -0600

    feat: order_shift_amounts method for Cracker

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -9,7 +9,8 @@ class Cracker < Cryptor
 
   def self.crack_key(cipher, date)
     offsets = (date.to_i ** 2).digits.reverse[-4..-1]
-    shifter_key = get_shifter_key(cipher)
+    shift_amounts = get_shift_amounts(cipher)
+    ordered_shift_amounts = order_shift_amounts(shift_amounts, (cipher.length - 1) % 4)
   end
 
   def self.get_shift_amounts(cipher)
@@ -19,6 +20,11 @@ class Cracker < Cryptor
     expected_chars.map.with_index do |expected_char, i|
       character_set.index(cipher_last_four[i]) - character_set.index(expected_char)
     end
+  end
+
+  def self.order_shift_amounts(shift_amounts, last_cipher_index)
+    (3 - last_cipher_index).times{ shift_amounts.rotate! }
+    shift_amounts
   end
 
 
