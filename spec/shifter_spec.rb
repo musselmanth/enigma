d6c59d35200286f97e33803d8e00391c32967e3b
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 08:41:32 -0600

    test: adjust tests for shifter for new implementation that allows decryption and encryption

--- spec/shifter_spec.rb
+++ spec/shifter_spec.rb
@@ -1,35 +1,58 @@
 RSpec.describe Shifter do
-  let(:d_shifter) { Shifter.new(20) }
+  
+  context 'encrypt' do
+    let(:d_shifter) { Shifter.new(20) }
 
-  it 'exists' do
-    expect(d_shifter).to be_instance_of(Shifter)
+    it 'exists' do
+      expect(d_shifter).to be_instance_of(Shifter)
+    end
+
+    it 'has a shift' do
+      expect(d_shifter.shift).to eq(20)
+    end
+
+    it 'can shift a character' do
+      expect(d_shifter.shift_character("a")).to eq("u")
+      expect(d_shifter.shift_character("l")).to eq("e")
+    end
+
+    it 'doesnt shift characters not in the character set' do
+      expect(d_shifter.shift_character("!")).to eq("!")
+      expect(d_shifter.shift_character("6")).to eq("6")
+    end
+
+    it 'encrypts capitals as lowercase' do
+      expect(d_shifter.shift_character("L")).to eq("e")
+    end
+
+    it 'can factory itself for encryption' do
+      shifters = Shifter.generate_shifters("02715", "040895", :encrypt)
+      expect(shifters).to all(be_instance_of(Shifter))
+      expect(shifters[0].shift).to eq(3)
+      expect(shifters[1].shift).to eq(27)
+      expect(shifters[2].shift).to eq(73)
+      expect(shifters[3].shift).to eq(20)
+    end
+
   end
 
-  it 'has a shift' do
-    expect(d_shifter.shift).to eq(20)
-  end
+  context 'decrypt' do
+    let(:d_shifter) { Shifter.new(-20) }
 
-  it 'can shift a character' do
-    expect(d_shifter.encrypt_character("a")).to eq("u")
-    expect(d_shifter.encrypt_character("l")).to eq("e")
-  end
+    it 'can shift a character with a negative shift' do
+      expect(d_shifter.shift_character("u")).to eq("a")
+      expect(d_shifter.shift_character("e")).to eq("l")
+    end
 
-  it 'doesnt shift characters not in the character set' do
-    expect(d_shifter.encrypt_character("!")).to eq("!")
-    expect(d_shifter.encrypt_character("6")).to eq("6")
-  end
+    it 'can factory itself with negative shifts for decryption' do
+      shifters = Shifter.generate_shifters("02715", "040895", :decrypt)
+      expect(shifters).to all(be_instance_of(Shifter))
+      expect(shifters[0].shift).to eq(-3)
+      expect(shifters[1].shift).to eq(-27)
+      expect(shifters[2].shift).to eq(-73)
+      expect(shifters[3].shift).to eq(-20)
+    end
 
-  it 'encrypts capitals as lowercase' do
-    expect(d_shifter.encrypt_character("L")).to eq("e")
-  end
-
-  it 'can factory itself' do
-    shifters = Shifter.generate_shifters("02715", "040895")
-    expect(shifters).to all(be_instance_of(Shifter))
-    expect(shifters[0].shift).to eq(3)
-    expect(shifters[1].shift).to eq(27)
-    expect(shifters[2].shift).to eq(73)
-    expect(shifters[3].shift).to eq(20)
   end
 
 end
