4fe210a4bc7b7a9f69d7614ada191c0e95da3dfd
Thomas Musselman <tmusselman@gmail.com>
2022-08-04 22:09:30 -0600

    refactor: change encryptor.encrypt to only return the encrypted message

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,19 +1,16 @@
 class Encryptor
 
-  attr_reader :key, :date, :shifters
+  attr_reader :shifters
 
   def initialize(key, date)
-    @key = key
-    @date = date
     @shifters = Shifter.generate_shifters(key, date)
   end
 
   def encrypt(message)
     characters = message.split(//)
-    encrypted = characters.map.with_index do |character, i|
+    characters.map.with_index do |character, i|
       @shifters[i % 4].encrypt_character(character)
     end.join
-    { encryption: encrypted, key: @key, date: @date }
   end
 
 end
--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -3,27 +3,15 @@ RSpec.describe Encryptor do
 
   it 'exists and has attributes' do
     expect(encryptor).to be_an(Encryptor)
-    expect(encryptor.date).to eq("040895")
-    expect(encryptor.key).to eq("02715")
     expect(encryptor.shifters).to all(be_instance_of(Shifter))
   end
   
   it 'can encrypt a message with no edge cases' do
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
-    expect(encryptor.encrypt("hello world")).to eq(expected)
+    expect(encryptor.encrypt("hello world")).to eq("keder ohulw")
   end
 
   it 'can encrypt edge cases' do
-    expected = {
-      encryption: "keder!sprrdx?",
-      key: "02715",
-      date: "040895"
-    }
-    expect(encryptor.encrypt("Hello! WORLD?")).to eq(expected)
+    expect(encryptor.encrypt("Hello! WORLD?")).to eq("keder!sprrdx?")
   end
 
 end
