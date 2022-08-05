ec405ee8dc29d69c713c36736125ab532a40cf41
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 15:40:08 -0600

    test: reordering shift amounts based on index of shifters used in last 4 letters

--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Cracker do
   it 'can get a key for which letter was used for each shifter' do
     expect(Cracker.get_shift_amounts("keder ohulwthnw")).to eq([-7, 3, 0, 19])
   end
+
+  it 'can reorder the shift amounts so they match the index of the shifters' do
+    expect(Cracker.order_shift_amounts([-7, 3, 0, 19], 2)).to eq([3, 0, 19, -7])
+  end
 end
