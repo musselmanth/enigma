fe74d232379fe82382abd6fc2c85d80289461dd9
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 16:14:01 -0600

    test: possible_key_segments from raw_key_segments

--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -14,4 +14,9 @@ RSpec.describe Cracker do
   it 'can subtract the offsets from the shift amounts and make the results not negative' do
     expect(Cracker.get_raw_key_segments([1, 0, 2, 5], [3, 0, 19, -7])).to eq([2, 0, 17, 15])
   end
+
+  it 'can get the real key_segments from the raw_key_segments' do
+    expect(Cracker.get_possible_key_segments([2, 0, 17, 15])).to eq([[2, 29, 56, 83], [0, 27, 54, 81], [17, 44, 71, 98], [15, 42, 69, 96]])
+  end
+
 end
