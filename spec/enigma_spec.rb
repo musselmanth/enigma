8d391ee5bad90d54852f34f1643087dd77a79ac9
Thomas Musselman <tmusselman@gmail.com>
2022-08-04 22:37:15 -0600

    test: enigma to use default date of today

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Enigma do
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
   end
 
+  it 'can use todays date if one is not provided' do
+    allow(Time).to receive(:now).and_return(Time.new(1995, 8, 4))
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    expect(enigma.encrypt("hello world", "02715")).to eq(expected)
+  end
+
 end
