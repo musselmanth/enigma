b8466586ad6792b372b6a7b8d66b4394cfaee0a3
Thomas Musselman <tmusselman@gmail.com>
2022-08-04 22:02:04 -0600

    feat: encryptor initialize and encrypt

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -18,17 +18,8 @@ class Shifter
   end
 
   def self.generate_shifters(key, date)
-    key_segments = {a: key[0..1].to_i, b: key[1..2].to_i, c: key[2..3].to_i, d: key[3..4].to_i}
     date_key = (date.to_i ** 2).digits.reverse[-4..-1]
-    offsets = {a: date_key[0], b: date_key[1], c: date_key[2], d: date_key[3]}
-    {
-      a: Shifter.new(key_segments[:a] + offsets[:a]),
-      b: Shifter.new(key_segments[:b] + offsets[:b]),
-      c: Shifter.new(key_segments[:c] + offsets[:c]),
-      d: Shifter.new(key_segments[:d] + offsets[:d])
-    }
+    (0..3).map{ |i| Shifter.new(key[i..i+1].to_i + date_key[i]) }
   end
-
-  private
 
 end
