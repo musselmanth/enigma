4d78163cae2606c15f1f4c166fac2e880032ab6a
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 11:52:35 -0600

    feat: helper method #get_shifter_key and beginning layout of crack_key

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -2,4 +2,21 @@ require './lib/cryptor'
 
 class Cracker < Cryptor
 
+  def initialize(cipher, date)
+    key = crack_key(cipher_date)
+    super(cipher, key, date)
+  end
+
+  def self.crack_key(cipher, date)
+    offsets = (date.to_i ** 2).digits.reverse[-4..-1]
+    shifter_key = get_shifter_key(cipher)
+  end
+
+  def self.get_shifter_key(cipher)
+    expected_chars = [" ", "e", "n", "d"]
+    ((cipher.length - 4) % 4).times{ expected_chars.rotate! }
+    expected_chars
+  end
+
+
 end
