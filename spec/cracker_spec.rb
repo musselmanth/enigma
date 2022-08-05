f6837d94ca9744f4ad13a39d011f04f2d3aed726
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 11:51:31 -0600

    test: key_crack method(not working yet) and helper method

--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -1,3 +1,9 @@
 RSpec.describe Cracker do
-  
+  xit 'can crack a key with a cipher and date' do
+    expect(Cracker.crack_key("keder ohulwthnw", "040895")).to eq("02715")
+  end
+
+  it 'can get a key for which letter was used for each shifter' do
+    expect(Cracker.shifter_key("keder ohulwthnw")).to eq(["d", " ", "e", "n"])
+  end
 end
