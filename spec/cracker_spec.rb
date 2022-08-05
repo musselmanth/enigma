46c86f8df4247f41fba63db72c6a508159b83ba3
Thomas Musselman <tmusselman@gmail.com>
2022-08-05 15:51:47 -0600

    test: help method that gets raw_key_segments from shifts and offsets

--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Cracker do
   it 'can reorder the shift amounts so they match the index of the shifters' do
     expect(Cracker.order_shift_amounts([-7, 3, 0, 19], 2)).to eq([3, 0, 19, -7])
   end
+
+  it 'can subtract the offsets from the shift amounts and make the results not negative' do
+    expect(Cracker.get_raw_key_segments([1, 0, 2, 5], [3, 0, 19, -7])).to eq([2, 0, 17, 15])
+  end
 end
