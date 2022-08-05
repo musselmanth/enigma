37244552a14ad39ef248e463d58e22cee9545acf
Thomas Musselman <tmusselman@gmail.com>
2022-08-04 22:01:32 -0600

    test: additional tests for encryptor method with new setup using shifters

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -1,13 +1,29 @@
 RSpec.describe Encryptor do
-  let(:encryptor) {Encryptor.new}
+  let(:encryptor) {Encryptor.new("02715", "040895")}
 
-  xit 'can encrypt a message with no edge cases' do
+  it 'exists and has attributes' do
+    expect(encryptor).to be_an(Encryptor)
+    expect(encryptor.date).to eq("040895")
+    expect(encryptor.key).to eq("02715")
+    expect(encryptor.shifters).to all(be_instance_of(Shifter))
+  end
+  
+  it 'can encrypt a message with no edge cases' do
     expected = {
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
     }
-    expect(encryptor.encrypt("hello world", "02715", "040895")).to eq(expected)
+    expect(encryptor.encrypt("hello world")).to eq(expected)
+  end
+
+  it 'can encrypt edge cases' do
+    expected = {
+      encryption: "keder!sprrdx?",
+      key: "02715",
+      date: "040895"
+    }
+    expect(encryptor.encrypt("Hello! WORLD?")).to eq(expected)
   end
 
 end
