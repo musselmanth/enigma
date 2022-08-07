6ff9e1479297b6433bda993d3c494bc9b4e55ab6
Thomas Musselman <tmusselman@gmail.com>
2022-08-07 12:05:01 -0600

    refactor: make methods instance methods now that they are funtioning properly together'

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -3,21 +3,21 @@ require './lib/cryptor'
 class Cracker < Cryptor
 
   def initialize(cipher, date)
-    key = crack_key(cipher_date)
+    key = crack_key(cipher, date)
     super(cipher, key, date)
   end
 
-  def self.crack_key(cipher, date)
+  def crack_key(cipher, date)
     offsets = (date.to_i ** 2).digits.reverse[-4..-1]
-    shift_amounts = Cracker.get_shift_amounts(cipher)
-    ordered_shift_amounts = Cracker.order_shift_amounts(shift_amounts, (cipher.length - 1) % 4)
-    raw_key_segments = Cracker.get_raw_key_segments(offsets, ordered_shift_amounts)
-    possible_key_segments = Cracker.get_possible_key_segments(raw_key_segments)
-    matching_key_segments = Cracker.get_matching_key_segments(possible_key_segments)
-    Cracker.combine_key_segments(matching_key_segments)
+    shift_amounts = get_shift_amounts(cipher)
+    ordered_shift_amounts = order_shift_amounts(shift_amounts, (cipher.length - 1) % 4)
+    raw_key_segments = get_raw_key_segments(offsets, ordered_shift_amounts)
+    possible_key_segments = get_possible_key_segments(raw_key_segments)
+    matching_key_segments = get_matching_key_segments(possible_key_segments)
+    combine_key_segments(matching_key_segments)
   end
 
-  def self.get_shift_amounts(cipher)
+  def get_shift_amounts(cipher)
     character_set = ("a".."z").to_a << " "
     expected_chars = [" ", "e", "n", "d"]
     cipher_last_four = cipher.split(//)[-4..-1]
@@ -26,17 +26,17 @@ class Cracker < Cryptor
     end
   end
 
-  def self.order_shift_amounts(shift_amounts, last_cipher_index)
+  def order_shift_amounts(shift_amounts, last_cipher_index)
     (3 - last_cipher_index).times{ shift_amounts.rotate! }
     shift_amounts
   end
 
-  def self.get_raw_key_segments(offsets, shifts)
+  def get_raw_key_segments(offsets, shifts)
     results = shifts.map.with_index{ |shift, i| shift - offsets[i] }
     results.map{ |result| result < 0 ? result + 27 : result }
   end
 
-  def self.get_possible_key_segments(raw_key_segments)
+  def get_possible_key_segments(raw_key_segments)
     possible_key_segments = [[],[],[],[]]
     raw_key_segments.each_with_index do |key_seg, i|
       possible_key_seg = key_seg
@@ -48,14 +48,14 @@ class Cracker < Cryptor
     possible_key_segments
   end
 
-  def self.get_matching_key_segments(possible_key_segments)
+  def get_matching_key_segments(possible_key_segments)
     solution = []
     possible_key_segments[0].each do |a_key|
       possible_key_segments[1].each do |b_key|
         possible_key_segments[2].each do |c_key|
           possible_key_segments[3].each do |d_key|
             possible_solution = [a_key, b_key, c_key, d_key]
-            solution = possible_solution if Cracker.is_key_solution?(possible_solution)
+            solution = possible_solution if is_key_solution?(possible_solution)
           end
         end
       end
@@ -63,19 +63,19 @@ class Cracker < Cryptor
     solution
   end
 
-  def self.is_key_solution?(key_segments)
-    Cracker.is_matching_segments?(key_segments[0], key_segments[1]) &&
-    Cracker.is_matching_segments?(key_segments[1], key_segments[2]) &&
-    Cracker.is_matching_segments?(key_segments[2], key_segments[3])
+  def is_key_solution?(key_segments)
+    is_matching_segments?(key_segments[0], key_segments[1]) &&
+    is_matching_segments?(key_segments[1], key_segments[2]) &&
+    is_matching_segments?(key_segments[2], key_segments[3])
   end
 
-  def self.is_matching_segments?(segment1, segment2)
+  def is_matching_segments?(segment1, segment2)
     segment1 = segment1.to_s.rjust(2, "0")
     segment2 = segment2.to_s.rjust(2, "0")
     segment1[-1] == segment2[0]
   end
 
-  def self.combine_key_segments(key_segments)
+  def combine_key_segments(key_segments)
     str_key_segments = key_segments.map{|segment| segment.to_s.rjust(2, "0")}
     "#{str_key_segments[0]}#{str_key_segments[1][1]}#{str_key_segments[2][1]}#{str_key_segments[3][1]}"
   end
--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -1,46 +1,48 @@
 RSpec.describe Cracker do
+  let(:cracker) {Cracker.new("keder ohulwthnw", "040895")}
+
   it 'can crack a key with a cipher and date' do
-    expect(Cracker.crack_key("keder ohulwthnw", "040895")).to eq("02715")
+    expect(cracker.crack_key("keder ohulwthnw", "040895")).to eq("02715")
   end
 
   it 'can get a key for which letter was used for each shifter' do
-    expect(Cracker.get_shift_amounts("keder ohulwthnw")).to eq([-7, 3, 0, 19])
+    expect(cracker.get_shift_amounts("keder ohulwthnw")).to eq([-7, 3, 0, 19])
   end
 
   it 'can reorder the shift amounts so they match the index of the shifters' do
-    expect(Cracker.order_shift_amounts([-7, 3, 0, 19], 2)).to eq([3, 0, 19, -7])
+    expect(cracker.order_shift_amounts([-7, 3, 0, 19], 2)).to eq([3, 0, 19, -7])
   end
 
   it 'can subtract the offsets from the shift amounts and make the results not negative' do
-    expect(Cracker.get_raw_key_segments([1, 0, 2, 5], [3, 0, 19, -7])).to eq([2, 0, 17, 15])
+    expect(cracker.get_raw_key_segments([1, 0, 2, 5], [3, 0, 19, -7])).to eq([2, 0, 17, 15])
   end
 
   it 'can get the real key_segments from the raw_key_segments' do
-    expect(Cracker.get_possible_key_segments([2, 0, 17, 15])).to eq([[2, 29, 56, 83], [0, 27, 54, 81], [17, 44, 71, 98], [15, 42, 69, 96]])
+    expect(cracker.get_possible_key_segments([2, 0, 17, 15])).to eq([[2, 29, 56, 83], [0, 27, 54, 81], [17, 44, 71, 98], [15, 42, 69, 96]])
   end
 
   it 'can take possible_key_segments and get the real ones' do
     input = [[2, 29, 56, 83], [0, 27, 54, 81], [17, 44, 71, 98], [15, 42, 69, 96]]
-    expect(Cracker.get_matching_key_segments(input)).to eq([2, 27, 71, 15])
+    expect(cracker.get_matching_key_segments(input)).to eq([2, 27, 71, 15])
   end
 
   it 'can test if two key segments are matching' do
-    expect(Cracker.is_matching_segments?(2, 27)).to be true
-    expect(Cracker.is_matching_segments?(45, 57)).to be true
-    expect(Cracker.is_matching_segments?(12, 2)).to be false
-    expect(Cracker.is_matching_segments?(12, 14)).to be false
+    expect(cracker.is_matching_segments?(2, 27)).to be true
+    expect(cracker.is_matching_segments?(45, 57)).to be true
+    expect(cracker.is_matching_segments?(12, 2)).to be false
+    expect(cracker.is_matching_segments?(12, 14)).to be false
   end
 
   it 'can confirm whether a set of key segments is a valid solution' do
-    expect(Cracker.is_key_solution?([2, 27, 71, 15])).to be true
-    expect(Cracker.is_key_solution?([12, 25, 51, 18])).to be true
-    expect(Cracker.is_key_solution?([12, 2, 27, 71])).to be false
-    expect(Cracker.is_key_solution?([15, 64, 34, 46])).to be false
+    expect(cracker.is_key_solution?([2, 27, 71, 15])).to be true
+    expect(cracker.is_key_solution?([12, 25, 51, 18])).to be true
+    expect(cracker.is_key_solution?([12, 2, 27, 71])).to be false
+    expect(cracker.is_key_solution?([15, 64, 34, 46])).to be false
   end
 
   it 'can combine matching key segments into a key' do
-    expect(Cracker.combine_key_segments([2, 27, 71, 15])).to eq("02715")
-    expect(Cracker.combine_key_segments([12, 23, 36, 61])). to eq("12361")
+    expect(cracker.combine_key_segments([2, 27, 71, 15])).to eq("02715")
+    expect(cracker.combine_key_segments([12, 23, 36, 61])). to eq("12361")
   end
 
 end
