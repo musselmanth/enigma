2df5ac94dc8be98fc2d9e6df8f9d09b3115fc48b
Thomas Musselman <tmusselman@gmail.com>
2022-08-08 14:55:07 -0600

    refactor: remove unneeded code and fix error with initial key segments being negative

--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe Cracker do
   end
 
   it 'can subtract the offsets from the shift amounts and make the results not negative' do
-    expect(cracker.get_raw_key_segments([1, 0, 2, 5], [3, 0, 19, -7])).to eq([2, 0, 17, 15])
+    expect(cracker.get_lowest_key_segments([1, 0, 2, 5], [3, 0, 19, -7])).to eq([2, 0, 17, 15])
   end
 
   it 'can get the real key_segments from the raw_key_segments' do
-    expect(cracker.get_possible_key_segments([2, 0, 17, 15])).to eq([[2, 29, 56, 83], [0, 27, 54, 81], [17, 44, 71, 98], [15, 42, 69, 96]])
+    expect(cracker.get_all_possible_key_segments([2, 0, 17, 15])).to eq([[2, 29, 56, 83], [0, 27, 54, 81], [17, 44, 71, 98], [15, 42, 69, 96]])
   end
 
   it 'can take possible_key_segments and get the real ones' do
