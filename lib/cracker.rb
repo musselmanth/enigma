6776cf7da7d190e24b8b81f8c3fd48c9f9a8179c
Thomas Musselman <tmusselman@gmail.com>
2022-08-08 17:52:17 -0600

    refactor: remove redundant helper methods and adjust other methods to accomodate

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -17,6 +17,7 @@ class Cracker < Cryptor
     lowest_key_segments = get_lowest_key_segments(offsets, ordered_shift_amounts)
     possible_key_segments = get_all_possible_key_segments(lowest_key_segments)
     matching_key_segments = get_matching_key_segments(possible_key_segments)
+    (puts "Cannot Crack."; exit) if matching_key_segments.empty?
     combine_key_segments(matching_key_segments)
   end
 
@@ -47,7 +48,7 @@ class Cracker < Cryptor
     lowest_key_segments.map.with_index do |key_seg, i|
       possible_key_segs = []
       until key_seg.digits.length > 2
-        possible_key_segs << key_seg
+        possible_key_segs << key_seg.to_s.rjust(2, "0")
         key_seg += 27
       end
       possible_key_segs
@@ -68,22 +69,15 @@ class Cracker < Cryptor
     end
     solution
   end
-
-  def is_key_solution?(key_segments)
-    is_matching_segments?(key_segments[0], key_segments[1]) &&
-    is_matching_segments?(key_segments[1], key_segments[2]) &&
-    is_matching_segments?(key_segments[2], key_segments[3])
-  end
-
-  def is_matching_segments?(segment1, segment2)
-    segment1 = segment1.to_s.rjust(2, "0")
-    segment2 = segment2.to_s.rjust(2, "0")
-    segment1[-1] == segment2[0]
+  
+  def is_key_solution?(key_segs)
+    key_segs[0][1] == key_segs[1][0] &&
+    key_segs[1][1] == key_segs[2][0] &&
+    key_segs[2][1] == key_segs[3][0]
   end
 
   def combine_key_segments(key_segments)
-    str_key_segments = key_segments.map{|segment| segment.to_s.rjust(2, "0")}
-    "#{str_key_segments[0]}#{str_key_segments[1][1]}#{str_key_segments[2][1]}#{str_key_segments[3][1]}"
+    "#{key_segments[0]}#{key_segments[1][1]}#{key_segments[2][1]}#{key_segments[3][1]}"
   end
 
 end
